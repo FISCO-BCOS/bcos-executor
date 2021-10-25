401393592ee7c249c5b13d55974e4a8c8ae300bc
morebtcg <651932351@qq.com>
2021-10-25 10:46:34 +0800

    Update comment

--- src/executor/TransactionExecutor.cpp
+++ src/executor/TransactionExecutor.cpp
@@ -456,7 +456,7 @@ void TransactionExecutor::executeTransaction(bcos::protocol::ExecutionMessage::U
     std::function<void(bcos::Error::UniquePtr, bcos::protocol::ExecutionMessage::UniquePtr)>
         callback)
 {
-    // EXECUTOR_LOG(DEBUG) << "ExecuteTransaction request" << LOG_KV("ContextID",
+    // EXECUTOR_LOG(TRACE) << "ExecuteTransaction request" << LOG_KV("ContextID",
     // input->contextID())
     //                     << LOG_KV("seq", input->seq()) << LOG_KV("Message type", input->type())
     //                     << LOG_KV("To", input->to()) << LOG_KV("Create", input->create());
