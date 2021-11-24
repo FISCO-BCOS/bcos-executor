c6f4507b414a1c7bf0e69fd3a8fe57ede7e77aad
morebtcg <651932351@qq.com>
2021-11-24 11:27:54 +0800

    Set read only when nextBlockHeader

--- src/executor/TransactionExecutor.cpp
+++ src/executor/TransactionExecutor.cpp
@@ -150,8 +150,8 @@ void TransactionExecutor::nextBlockHeader(const bcos::protocol::BlockHeader::Con
                     return;
                 }
 
+                prev.storage->setReadOnly(true);
                 stateStorage = std::make_shared<bcos::storage::StateStorage>(prev.storage);
-                prev.storage->setCachePrev(false);
             }
 
             m_blockContext = createBlockContext(blockHeader, stateStorage);
